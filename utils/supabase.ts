25e7e4dcb1e4c143f3f11244d543ce40dc7ddf15
lllogggs <159713119+lllogggs@users.noreply.github.com>
2026-02-18 04:21:33 +0900

    Update supabase.ts

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import { Database } from '../types';
 
-// process.env로 변경하고, Vercel 환경변수 이름에 맞춰주세요
+// Vercel 환경변수 이름에 맞춰서 가져옵니다.
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.NEXT_PUBLIC_VITE_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.NEXT_PUBLIC_VITE_SUPABASE_ANON_KEY!;
 
